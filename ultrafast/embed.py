323535aabb16aed0d537f4a17c20d505dcd304dc
drewnutt <anm329@pitt.edu>
2024-09-27 11:30:59 -0400

    user configurable number of workers for embed

--- ultrafast/embed.py
+++ ultrafast/embed.py
@@ -15,10 +15,10 @@ def embed_cli():
     parser = argparse.ArgumentParser(description='Generate embeddings from DrugTargetCoembeddingLightning model')
     parser.add_argument('--data-file', type=str, required=True, help='Path to file containing molecules to embed, in tsv format. With header and columns: "SMILES" for drugs, "Target Sequence" for targets')
     parser.add_argument("--moltype", type=str, help="Molecule type", choices=["drug", "target"], default="target")
-
     parser.add_argument('--checkpoint', type=str, required=True, help='Path to model checkpoint')
     parser.add_argument('--output_path', type=str, required=True, help='path to save embeddings. Currently only supports numpy format.')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size for inference')
+    parser.add_argument('--num-workers', type=int, default=-1, help='Number of processes for featurization and DataLoading')
     parser.add_argument('--device', type=str, default=0, help='CUDA device. If CUDA is not available, this will be ignored.')
     parser.add_argument('--ext', type=str, default="h5", choices=['h5', 'lmdb', 'pt'], help='File format to store the drug and target features before co-embedding.')
     args = parser.parse_args()
@@ -33,6 +33,7 @@ def embed(
     output_path: str,
     batch_size: int,
     ext: str,
+    num_workers: int,
 ):
     args = argparse.Namespace(
         checkpoint=checkpoint,
@@ -42,6 +43,7 @@ def embed(
         output_path=output_path,
         batch_size=batch_size,
         ext=ext,
+        num_workers=num_workers,
     )
 
     model = DrugTargetCoembeddingLightning.load_from_checkpoint(args.checkpoint)
@@ -54,7 +56,7 @@ def embed(
     if os.path.dirname(args.output_path) != '' and not os.path.exists(os.path.dirname(args.output_path)):
         os.makedirs(os.path.dirname(args.output_path))
     if args.moltype == "drug":
-        featurizer = get_featurizer(model.args.drug_featurizer, batch_size=args.batch_size, save_dir=os.path.dirname(args.output_path), ext=ext)
+        featurizer = get_featurizer(model.args.drug_featurizer, batch_size=args.batch_size, save_dir=os.path.dirname(args.output_path), ext=ext, n_jobs=args.num_workers)
     elif args.moltype == "target":
         featurizer = get_featurizer(model.args.target_featurizer, batch_size=args.batch_size, save_dir=os.path.dirname(args.output_path), ext=ext)
     featurizer = featurizer.to(device)
@@ -62,7 +64,7 @@ def embed(
     dataset = EmbedDataset(args.data_file, args.moltype, featurizer)
 
     collate_fn = partial(embed_collate_fn, moltype=args.moltype)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn, num_workers=args.num_workers)
 
     embeddings = []
     with torch.no_grad():
